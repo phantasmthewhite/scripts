ae44d6b45e04caa2c8e993288d3582ca607fdfed
phantasmthewhite <46378265+phantasmthewhite@users.noreply.github.com>
2021-03-25 10:06:09 +0100

    Update kali_post_install.sh

--- kali_post_install.sh
+++ kali_post_install.sh
@@ -3,13 +3,21 @@
 #Custom ZSHRC
 curl https://raw.githubusercontent.com/phantasmthewhite/scripts/main/zshrc | base64 -d > ~/.zshrc
 
+#Install various tools
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install git
 sudo apt-get install default-jdk
 sudo apt-get install terminator
 sudo apt-get install fzf
 sudo apt-get install tree
-sudo apt install powershell-empire
+sudo apt-get install powershell-empire
+
+#Install SublimeText
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
 
 #Install Ghidra
 git clone https://github.com/NationalSecurityAgency/ghidra.git /opt/
